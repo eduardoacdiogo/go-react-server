10f00b524492641161e0bdbb550516572a4f2d99
Eduardo Diogo <educdiogo@hotmail.com>
2024-08-08 19:40:05 -0400

    feature: created api

---
+++ cmd/tools/terndotenv/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
